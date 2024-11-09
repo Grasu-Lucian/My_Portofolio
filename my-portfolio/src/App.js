c5f6ae31c4e824861261e44f0db24c95ae0fd35e
erro0rdude <grasu.lucian20@yahoo.com>
2024-11-09 15:08:32 +0200

    Refactor App styles by removing margin color transitions and adjusting background color for improved clarity

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className={`App ${isNightMode ? 'dark' : ''}`}>
       <Navbar toggleTheme={toggleTheme} isNightMode={isNightMode} />
-      <div className="pt-16 p-4 bg-background dark:bg-night text-primary dark:text-light margin-color">
+      <div className="pt-16 p-4 bg-background dark:bg-night text-primary dark:text-light">
         <AboutMe />
         {/* Other components or content can go here */}
       </div>
