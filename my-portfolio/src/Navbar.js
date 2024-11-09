24ff27e40dc630078de6d3e8ce448eed33bcb165
erro0rdude <grasu.lucian20@yahoo.com>
2024-11-09 13:43:05 +0200

    Add Google Fonts link and update Navbar title

--- my-portfolio/src/Navbar.js
+++ my-portfolio/src/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Navbar.css';
 
 function Navbar({ toggleTheme, isNightMode }) {
   return (
     <nav className="navbar fixed top-0 left-0 w-full bg-primary text-white flex justify-between items-center p-4">
-      <div className="text-lg font-bold">My Portfolio</div>
+      <div className="text-lg font-bold">Grasu Lucian</div>
       <ul className="flex space-x-4 items-center">
         <li><a href="#home" className="hover:bg-secondary p-2 rounded transition duration-300 ease-in-out">Home</a></li>
         <li><a href="#about" className="hover:bg-secondary p-2 rounded transition duration-300 ease-in-out">About</a></li>
