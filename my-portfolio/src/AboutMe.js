8a59184a24d98d51eb01964dc21245cb5dd961f4
erro0rdude <grasu.lucian20@yahoo.com>
2024-11-09 14:51:58 +0200

    Enhance AboutMe section with responsive design, improved styles, and padding adjustments

--- my-portfolio/src/AboutMe.js
+++ my-portfolio/src/AboutMe.js
@@ -6,13 +6,13 @@ function AboutMe() {
   return (
     <section className="about-me">
       <div className="about-me-content">
-        <h2>Hello! Welcome to my portfolio</h2>
-        <p>
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-4">Hello! Welcome to my portfolio</h2>
+        <p className="text-sm sm:text-base md:text-lg leading-relaxed">
           Hello! I'm a student at the Bucharest University of Economic Studies (ASE) in Romania, with a passion for creating projects that connect with the IT field. I enjoy building websites, developing APIs and diving into cybersecurity. My goal with this portfolio is to showcase these projects, highlight my learning journey, and demonstrate my dedication to mastering new skills in development and security.
         </p>
       </div>
       <div className="about-me-image">
-        <img src={meImage} alt="Me" />
+        <img src={meImage} alt="Me" className="max-w-full h-auto rounded-full" />
       </div>
     </section>
   );
