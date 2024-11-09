7f9c71ffecf0865720adcaa3ff3fe35981c30819
erro0rdude <grasu.lucian20@yahoo.com>
2024-11-09 20:35:44 +0200

    Update ContactMe component to include form action and name attributes for email and message fields

--- my-portfolio/src/ContactMe.js
+++ my-portfolio/src/ContactMe.js
@@ -8,18 +8,18 @@ function ContactMe() {
       <p className="mb-4">
         Do you have a project in your mind that you want to be brought to life? Do you need a partner? Looking for a front-end developer? Or do you have some feedback for me? Send me a message!
       </p>
-      <form className="contact-form">
-        <div className="form-group">
-          <label htmlFor="name">Your Name</label>
-          <input type="text" id="name" placeholder="Write your name" required />
-        </div>
+      <form
+        className="contact-form"
+        action="https://formspree.io/f/xeoqkqpl"
+        method="POST"
+      >
         <div className="form-group">
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" placeholder="Email Address" required />
+          <input type="email" id="email" name="email" placeholder="Email Address" required />
         </div>
         <div className="form-group">
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" placeholder="Write your message here" required></textarea>
+          <textarea id="message" name="message" placeholder="Write your message here" required></textarea>
         </div>
         <button type="submit" className="send-button">Send</button>
       </form>
